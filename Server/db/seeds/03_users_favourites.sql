ca325400d3588cd5476ff9cde6e1f85a2778900b
keepitclassick <keepitclassick@gmail.com>
2022-01-14 14:51:40 -0500

    add users_favourites seeds

---
+++ Server/db/seeds/03_users_favourites.sql
@@ -0,0 +1,3 @@
+INSERT INTO users_favourites (users_id, shelters_id, pets_id) VALUES (1, 3, 124);
+INSERT INTO users_favourites (users_id, shelters_id, pets_id) VALUES (2, 2, 136);
+INSERT INTO users_favourites (users_id, shelters_id, pets_id) VALUES (1, 1, 174);
