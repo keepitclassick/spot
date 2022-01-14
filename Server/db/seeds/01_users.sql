dfa16e899d5162c04588e8557e154fb2d2f11126
keepitclassick <keepitclassick@gmail.com>
2022-01-14 14:39:03 -0500

    add db reset script and users seeds

---
+++ Server/db/seeds/01_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, phone_number, password, favourited_pets, location)
+VALUES 
+
+('Azamat Wong','AzamatWong@gmail.com','519-123-4567', 'password', '','Whitby, ON, Canada' ),
+('Eiran Griffiths','EiranGriffiths@gmail.com','519-123-4567','password', '', 'Toronto, ON, Canada'),
+('Lucia Ennis','LuciaEnnis@gmail.com','519-123-4567','password','', 'London, ON, Canada'),
+('Iphigenia Corra', 'IphigeniaCorra@gmail.com','519-123-4567','password','', 'Chatham, ON, Canada'),
+('Nazim Frost', 'SnicimFrost@gmail.com','519-123-4567','password', '', 'Toronto, ON, Canada');
