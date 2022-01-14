b21d0ff776ed0b784547afb131441baaf3275dfa
keepitclassick <keepitclassick@gmail.com>
2022-01-14 14:44:51 -0500

    add shelters seeds

--- Server/db/seeds/02_shelters.sql
+++ Server/db/seeds/02_shelters.sql
@@ -1,8 +1,6 @@
-INSERT INTO users (name, email, phone_number, password, favourited_pets, location)
+INSERT INTO shelters (name, email, phone_number, password, favourited_pets, location)
 VALUES 
 
-('Azamat Wong','AzamatWong@gmail.com','519-123-4567', 'password', '','Whitby, ON, Canada' ),
-('Eiran Griffiths','EiranGriffiths@gmail.com','519-123-4567','password', '', 'Toronto, ON, Canada'),
-('Lucia Ennis','LuciaEnnis@gmail.com','519-123-4567','password','', 'London, ON, Canada'),
-('Iphigenia Corra', 'IphigeniaCorra@gmail.com','519-123-4567','password','', 'Chatham, ON, Canada'),
-('Nazim Frost', 'SnicimFrost@gmail.com','519-123-4567','password', '', 'Toronto, ON, Canada');
+('Pet and Wildlife Rescue','pawr@gmail.com','226-996-9969', 'password', '','405 Park Ave E, Chatham, ON N7M 3W4'),
+('St. Thomas Animal Shelter','stas@gmail.com','519-123-4567','password', '', '100 Burwell Rd, St Thomas, ON N5P 1E5'),
+('Humane Society London & Middlesex','hslm@gmail.com','519-123-4567','password','', '624 Clarke Rd, London, ON N5V 3K5');
