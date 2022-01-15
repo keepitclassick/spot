3ca549e1e606804476eac4afaeb7dffa6a4fd219
Desiree Mendes <desireefrost@live.ca>
2022-01-15 11:01:16 -0500

    reinstall bootstrap due to error

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className='App'>
+			<Nav />
 			<Link to='/'>Home </Link>
 			<Routes>
 				<Route path='/' element={<h1>Hello</h1>} />
