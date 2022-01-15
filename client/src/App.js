f276553b7f872b46c8cfc3c7d4f05f603d69e4ad
Desiree Mendes <desireefrost@live.ca>
2022-01-15 13:38:31 -0500

    Import fonts, fix footer to bottom

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import { Carousel, Nav } from "react-bootstrap";
+import Footer from "./Components/Footer";
+import CarouselContainer from "./Components/CarouselContainer";
 
 function App() {
   return (
-    <div className='App'>
+    <div className='page-container'>
+			<div className="content-wrap">
 			<Nav />
 			<Link to='/'>Home </Link>
 			<Routes>
 				<Route path='/' element={<h1>Hello</h1>} />
 			</Routes>
+		</div>
+		<Footer />
 		</div>
   );
 }
