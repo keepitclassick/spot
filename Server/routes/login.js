92ad20d801238959d9b3927e7f6cbc1594319b47
Desiree Mendes <desireefrost@live.ca>
2022-01-15 18:39:26 -0500

    Install nodemailer as a dependency

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -3,10 +3,40 @@ const router = express.Router();
 
 
 module.exports = (db) => {
-  router.get("/login", function (req, res) {
-    res.send("testing.");
+  router.get("/", function (req, res) {
+    res.send("testing route");
   });
 
+  const getUserWithEmail = function (email) {
+    return db
+      .query(`SELECT * FROM users WHERE email = $1`, [email.toLowerCase()])
+      .then((result) => {
+        return result.rows[0];
+      })
+      .catch((err) => {
+        return null;
+      });
+  };
+  exports.getUserWithEmail = getUserWithEmail;
 
+  const login = function (userEmail, userPassword) {
+    return getUserWithEmail(userEmail).then((user) => {
+      if (!user) {
+       return null 
+       }
+      if (userPassword, user.password) {
+        return user;
+      }; 
+    });
+  };
+  exports.login = login;
+
+ router.post("/", function (req, res) {
+   const { email, password } = req.body;
+   login(email, password).then((user) => {
+     console.log("------", email)
+   })
+ })
+ 
   return router;
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Navigation from "./Components/Navigation";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { Carousel, Nav } from "react-bootstrap";
 import Footer from "./Components/Footer";
 import CarouselContainer from "./Components/CarouselContainer";
+import Login from "./Components/Login";
+// import Login  from "./Components/Login";
 
 function App() {
   return (
     <div className='page-container'>
 			<div className="content-wrap">
-			<Nav />
+			<Navigation />
+			<Login />
 			<Link to='/'>Home </Link>
 			<Routes>
 				<Route path='/' element={<h1>Hello</h1>} />
+				<Route path='/Login' element={Login} />
 			</Routes>
 		</div>
 		<Footer />
