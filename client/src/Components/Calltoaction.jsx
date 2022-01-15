107210e9aa88a83697921100b0f2bc2cfa17ddca
keepitclassick <keepitclassick@gmail.com>
2022-01-15 16:16:24 -0500

    add Calltoaction component with link to login

--- client/src/Components/Calltoaction.jsx
+++ client/src/Components/Calltoaction.jsx
@@ -8,11 +8,15 @@ export default function Banner() {
         <div class="col-sm-4"></div>
         <div class="col-sm-4"></div>
         <div class="col-sm-4">
-          <Image
-            className="mx-auto"
-            src={"./images/F6DCA019-89B6-4F5E-B5B2-5559FC36E363_4_5005_c.jpeg"}
-            fluid
-          />
+          <a href="/login">
+            <Image
+              className="mx-auto"
+              src={
+                "./images/F6DCA019-89B6-4F5E-B5B2-5559FC36E363_4_5005_c.jpeg"
+              }
+              fluid
+            />
+          </a>
         </div>
       </div>
     </Container>
