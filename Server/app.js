ece11e399035fcaf430fd5d3842a41fc330a0f1a
Desiree Mendes <desireefrost@live.ca>
2022-01-14 23:40:11 -0500

    Begin initializing routes

--- Server/app.js
+++ Server/app.js
@@ -3,13 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const db = require('./db');
+const app = express();
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
-const app = express();
 const adminRouter = require('./routes/admin');
-const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const logoutRouter = require('./routes/logout');
 const registerRouter = require('./routes/register');
@@ -24,12 +23,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 app.use('/api/admin', adminRouter(db));
 app.use("/api/login", loginRouter(db));
 app.use("/api/logout", logoutRouter(db));
 app.use("/api/register", registerRouter(db));
-app.use("/api/favourites", favouritesRouter(db));
+app.use("/favourites", favouritesRouter(db));
 app.use("/api/delete", deleteRouter(db));
 
 
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,2 +1,12 @@
 const express = require("express")
 const router = express.Router();
+
+
+module.exports = (db) => {
+  router.get("/login", function (req, res) {
+    res.send("testing.");
+  });
+
+
+  return router;
+};
