2d201a54eddd8e2cec5d8f86f0cae15e13ec2b84
keepitclassick <keepitclassick@gmail.com>
2022-01-15 13:43:57 -0500

    align nav links

--- client/src/Components/Navigation.jsx
+++ client/src/Components/Navigation.jsx
@@ -4,36 +4,38 @@ export default function Navigation() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand id="logo" href="#home">
-          <img
-            alt=""
-            src="./BA7E6C06-C68A-4B54-9CEC-3C39CAFB09CB_4_5005_c.jpeg"
-            width="80"
-            height="60"
-            className="d-inline-block align-top"
-          />{" "}
-          Spot
-        </Navbar.Brand>
-        <Nav id="links">
-          <Nav.Item>
-            <Nav.Link href="/home">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="/Adopt">Adopt</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="/Resources">Resources</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="/About">About</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="/Register">Register</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="/Login">Login</Nav.Link>
-          </Nav.Item>
-        </Nav>
+        <Container>
+          <Navbar.Brand id="logo" href="#home">
+            <img
+              alt=""
+              src="./BA7E6C06-C68A-4B54-9CEC-3C39CAFB09CB_4_5005_c.jpeg"
+              width="80"
+              height="60"
+              className="d-inline-block align-top"
+            />{" "}
+            Spot
+          </Navbar.Brand>
+          <Nav id="links" className="justify-content-end">
+            <Nav.Item>
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/Adopt">Adopt</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/Resources">Resources</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/About">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/Register">Register</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="/Login">Login</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
       </Navbar>
     </>
   );
