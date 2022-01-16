99cc9076876e7c0ec456e4996e9a4d8790c08861
Desiree Mendes <desireefrost@live.ca>
2022-01-15 21:44:05 -0500

    Initialize email functionality

--- Server/routes/email.js
+++ Server/routes/email.js
@@ -2,3 +2,35 @@ const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
 
+app.get('/', () => {
+resizeBy.send('Welcome to my email form')
+});
+
+app.post('/api/email', (req, res) => {
+let data = req.body
+let smtpTransport = nodemailer.createTransport({
+  service: 'Gmail',
+  port:465,
+  auth:{
+    user: 'desireemendes@icloud.com',
+    pass: 'nice-try'
+  }
+});
+
+let mailOptions = {
+  from:data.email,
+  to: 'desireemendes@icloud.com',
+  subject: `Message from ${data.name}`,
+  html: `
+  <h3>Information</h3>
+  <ul>
+  <li>Name: ${data.name}</li>
+  <li>Email: ${data.email}</li>
+  </ul>
+
+  <h3>Message</h3>
+  <p>${data.message}</p>
+  `
+
+}
+})
