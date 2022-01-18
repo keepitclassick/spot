eedf469a4c24f8760f6c6c9e70a2b460a8e1e14a
keepitclassick <keepitclassick@gmail.com>
2022-01-18 14:17:08 -0500

    add basic form for registration and create scss file

--- client/src/Components/Registration.jsx
+++ client/src/Components/Registration.jsx
@@ -1,5 +1,39 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
 export default function Registration() {
   return (
-    <h1>Register</h1>
-  )
-} 
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter you name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Location</Form.Label>
+        <Form.Control type="text" placeholder="Enter location" />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Cell Phone</Form.Label>
+        <Form.Control type="phone" placeholder="Enter phone number" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Confirm Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
