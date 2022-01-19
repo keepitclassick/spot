32d0df5ea3d900ae5058c6affe86a847951b87fd
Desiree Mendes <desireefrost@live.ca>
2022-01-19 15:10:56 -0500

    layout for resources page

--- client/src/Components/AboutUs.jsx
+++ client/src/Components/AboutUs.jsx
@@ -9,6 +9,11 @@ export default function AboutUs() {
     <h1>About Us</h1>
     <p class="text">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque non tellus. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Dui faucibus in ornare quam viverra orci sagittis eu volutpat. Molestie at elementum eu facilisis sed odio morbi quis. Elit pellentesque habitant morbi tristique senectus. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Id porta nibh venenatis cras sed felis eget. Aenean pharetra magna ac placerat. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Nisl vel pretium lectus quam id.
+
+      Spot is an app that helps you find an adoptable pet based on your lifestyle.
+      
+      Why Adopt?
+
     </p>
   </div>
 </div>
@@ -18,30 +23,19 @@ export default function AboutUs() {
     <Col>FAQ's</Col>
     <Accordion defaultActiveKey="0">
   <Accordion.Item eventKey="0">
-    <Accordion.Header>Question #1</Accordion.Header>
+    <Accordion.Header>I've found a pet that I'm interested in, what's next?</Accordion.Header>
     <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Fill out the contact form, found under the "Contact" tab and we'll get you in touch directly with the shelter!
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="1">
-    <Accordion.Header>Question #2</Accordion.Header>
+    <Accordion.Header>How much will it cost?</Accordion.Header>
     <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Pet adoption fees vary from shelter to shelter. These fees are based on training, spay/neuter, housing, veterinary/medical care. Get in contact with 
+      us and we'll assist you in the adoption process.
     </Accordion.Body>
   </Accordion.Item>
-  <Accordion.Item eventKey="1">
+  <Accordion.Item eventKey="2">
     <Accordion.Header>Question #3</Accordion.Header>
     <Accordion.Body>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -53,7 +47,7 @@ export default function AboutUs() {
       est laborum.
     </Accordion.Body>
   </Accordion.Item>
-  <Accordion.Item eventKey="1">
+  <Accordion.Item eventKey="3">
     <Accordion.Header>Question #4</Accordion.Header>
     <Accordion.Body>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
--- client/src/Components/Resources.jsx
+++ client/src/Components/Resources.jsx
@@ -1,5 +1,51 @@
+import React, { Fragment } from "react"
+import { Row, Card, Col, CardGroup } from "react-bootstrap"
 export default function Resources() {
   return (
-    <h1>Resources</h1>
+   
+    <React.Fragment>
+      <div className="title">
+      <h1>Resources</h1>
+      </div>
+      <div class="container">
+  <div className="row">
+    <div className="col-sm">
+      <div class="card" >
+  <img class="card-img-top" src="https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="Card image cap"
+  height="300px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col-sm">
+      <div class="card">
+  <img class="card-img-top" src="https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAyfHxkb2dzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="Card image cap"
+  height="300px" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col-sm">
+     <div class="card">
+  <img class="card-img-top" src="https://images.unsplash.com/photo-1516139008210-96e45dccd83b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHBldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="Card image cap"
+  height="300px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+    </React.Fragment>
   )
+
+ 
 }
