41df77216e3bf881f955d17b9ba8fbf7ed133ee2
keepitclassick <keepitclassick@gmail.com>
2022-01-18 20:07:40 -0500

    Add accordion to pet listing

--- client/src/Components/Pet.js
+++ client/src/Components/Pet.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./AdoptStyles.scss";
+import { Accordion } from "react-bootstrap";
 
 const Pet = ({
   id,
@@ -27,6 +28,20 @@ const Pet = ({
           <a href={url} class="btn btn-dark">
             Meet {name}
           </a>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Accordion Item #1</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </span>
       </div>
     </div>
