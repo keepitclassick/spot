fc755978ab5f906948e3cf35c8a0c2b3a66b686a
Desiree Mendes <desireefrost@live.ca>
2022-01-20 21:54:01 -0500

    center text at bottom

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -7,12 +7,74 @@ export default function Home() {
   return (
     <>
     {/* Slogan */}
+    <section class="colored-section" id="cta">
     <div class="row">
 
         <div class="col-lg-6">
           <h1 class="big-heading">See spot. Love spot. Adopt spot.</h1>
         </div>
         </div>
+        {/* Pet cards */}
+
+        <div class="row-md"></div>
+
+<div id="examplepets" class="row-md-6">
+  <div class="card-group">
+    <div class="card">
+      <img
+        class="card-img-top"
+        src="./images/petexample1.jpeg"
+        height="fit-content"
+        alt="black dog"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Sausage</h5>
+        <p class="card-text">
+          Sausage is around 4 to 5 years old and 17 pounds. She is very
+          social with people and other dogs. She is playful but not
+          hyper.
+        </p>
+      </div>
+      <div class="card-footer text-center"></div>
+    </div>
+    <div class="card">
+      <img
+        class="card-img-top"
+        src="./images/examplepet2.jpeg"
+        height="fit-content"
+        alt="black and white cat"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Lester Sylvester</h5>
+        <p class="card-text">
+          Sweet and intelligent, he is a responsive lad. When you are
+          speaking to him, he will chat you back. Sylvester has
+          personality!
+        </p>
+      </div>
+      <div class="card-footer text-center"></div>
+    </div>
+    <div class="card">
+      <img
+        class="card-img-top"
+        src="./images/petexample3.jpeg"
+        height="fit-content"
+        alt="orange cat"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Gingerbread</h5>
+        <p class="card-text">
+          This playful boy loves plush toys and sparkly balls. He enjoys
+          chasing his siblings and cuddling with his foster dad.
+        </p>
+      </div>
+      <div class="card-footer text-center"></div>
+    </div>
+  </div>
+  </div>
+  
+
+        </section>
 
         {/* Features */}
         <section class="white-section" id="features">
@@ -40,6 +102,17 @@ export default function Home() {
   </div>
 </div>
 </section>
+
+{/* Call to action */}
+<section class="colored-section" id="cta">
+
+<div class="container-fluid">
+  <h3 class="big-heading">Find a pet to adopt today</h3>
+</div>
+</section>
+
+
+
 
 
    </>
