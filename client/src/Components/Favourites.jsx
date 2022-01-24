80480c7a54ce33c4f00a459687b394cb78c40262
keepitclassick <keepitclassick@gmail.com>
2022-01-24 13:09:56 -0500

    change styling of favourites

--- client/src/Components/Favourites.jsx
+++ client/src/Components/Favourites.jsx
@@ -7,107 +7,115 @@ export default function Favourites() {
   return (
     <div className="pet">
       <h1>Favourites</h1>
-      {Object.keys(petsFromLocalStorage).map(function (key) {
-        const img = petsFromLocalStorage[key].media[0].medium;
-        const description =
-          petsFromLocalStorage[key].tags.length > 2
-            ? `Hi! I'm ${petsFromLocalStorage[key].name} the ${petsFromLocalStorage[key].gender} ${petsFromLocalStorage[key].type}. Besides being adorable, I'm also 
+      <div className="pets__wrapper" data-testid="pets-list-id">
+        {Object.keys(petsFromLocalStorage).map(function (key) {
+          const img = petsFromLocalStorage[key].media[0].medium;
+          const description =
+            petsFromLocalStorage[key].tags.length > 2
+              ? `Hi! I'm ${petsFromLocalStorage[key].name} the ${petsFromLocalStorage[key].gender} ${petsFromLocalStorage[key].type}. Besides being adorable, I'm also 
     ${petsFromLocalStorage[key].tags[0]}, ${petsFromLocalStorage[key].tags[1]} and ${petsFromLocalStorage[key].tags[2]}.`
-            : `Hi! I'm ${petsFromLocalStorage[key].name} the ${petsFromLocalStorage[key].gender} ${petsFromLocalStorage[key].type}. Besides being adorable, I'm also 
+              : `Hi! I'm ${petsFromLocalStorage[key].name} the ${petsFromLocalStorage[key].gender} ${petsFromLocalStorage[key].type}. Besides being adorable, I'm also 
     affectionate, adventurous and loyal.`;
 
-        const kids =
-          petsFromLocalStorage[key].environment.children === true
-            ? "✅  Good with kids"
-            : "🚫  Not appropriate for a home with children";
-        const cats =
-          petsFromLocalStorage[key].environment.cats === true
-            ? "✅  Good with cats"
-            : "🚫  Not appropriate for a home with cats";
-        const dogs =
-          petsFromLocalStorage[key].environment.dogs === true
-            ? "✅  Good with dogs"
-            : "🚫  Not appropriate for a home with dogs";
+          const kids =
+            petsFromLocalStorage[key].environment.children === true
+              ? "✅  Good with kids"
+              : "🚫  Not appropriate for a home with children";
+          const cats =
+            petsFromLocalStorage[key].environment.cats === true
+              ? "✅  Good with cats"
+              : "🚫  Not appropriate for a home with cats";
+          const dogs =
+            petsFromLocalStorage[key].environment.dogs === true
+              ? "✅  Good with dogs"
+              : "🚫  Not appropriate for a home with dogs";
 
-        const houseTrained =
-          petsFromLocalStorage[key].attributes.house_trained === true
-            ? "✅  House trained"
-            : "🚫  Not house trained";
+          const houseTrained =
+            petsFromLocalStorage[key].attributes.house_trained === true
+              ? "✅  House trained"
+              : "🚫  Not house trained";
 
-        const shotsCurrent =
-          petsFromLocalStorage[key].attributes.shots_current === true
-            ? "✅  Shots current"
-            : "🚫  Shots not current";
+          const shotsCurrent =
+            petsFromLocalStorage[key].attributes.shots_current === true
+              ? "✅  Shots current"
+              : "🚫  Shots not current";
 
-        const spayedNeutered =
-          petsFromLocalStorage[key].attributes.spayed_neutered === true
-            ? "✅  Spayed/Neutered"
-            : "🚫  Not Spayed or Neutered";
+          const spayedNeutered =
+            petsFromLocalStorage[key].attributes.spayed_neutered === true
+              ? "✅  Spayed/Neutered"
+              : "🚫  Not Spayed or Neutered";
 
-        const specialNeeds =
-          petsFromLocalStorage[key].attributes.special_needs === true
-            ? "✅  Special Needs"
-            : "🚫  No Special Needs";
+          const specialNeeds =
+            petsFromLocalStorage[key].attributes.special_needs === true
+              ? "✅  Special Needs"
+              : "🚫  No Special Needs";
 
-        return (
-          <>
-            <tr key={key}>
-              <div className="pets__card" data-tesid="pets-comp">
-                <div className="pets__card-box">
-                  <img src={img} alt="" />
-                  <span id="name">
-                    <h5>{petsFromLocalStorage[key].name}</h5>
-                  </span>
-                  <span>
-                    <Accordion>
-                      <Accordion.Item eventKey="0">
-                        <Accordion.Header>
-                          <h6>More about {petsFromLocalStorage[key].name}</h6>
-                        </Accordion.Header>
-                        <Accordion.Body>
-                          <h3>{`🏡${petsFromLocalStorage[key].contact.address.city}, ${petsFromLocalStorage[key].contact.address.state}`}</h3>
-                          <h4>{`${petsFromLocalStorage[key].breeds} ${petsFromLocalStorage[key].type}`}</h4>
+          return (
+            <>
+              <tr key={key}>
+                <div className="pets__card" data-tesid="pets-comp">
+                  <div className="pets__card-box">
+                    <img src={img} alt="" />
+                    <span id="name">
+                      <h5>{petsFromLocalStorage[key].name}</h5>
 
-                          <h5>{`${description}`}</h5>
-                          <br />
-                          <u>
-                            <b>Environment:</b>
-                          </u>
-                          <br />
-                          {kids}
-                          <br />
-                          {cats}
-                          <br />
-                          {dogs}
-                          <br />
-                          <br />
-                          <u>
-                            <b>Good to know:</b>
-                          </u>
-                          <br />
-                          {houseTrained}
-                          <br />
-                          {shotsCurrent}
-                          <br />
-                          {spayedNeutered}
-                          <br />
-                          {specialNeeds}
-                          <div class="map"></div>
-                          <a
-                            href={`mailto:${petsFromLocalStorage[key].contact.email}?subject=Potential%20Adoption&body=I%20would%20like%20to%20have%20more%20information%20about%20${petsFromLocalStorage[key].name}.`}
-                          >
-                            <button class="btn btn-dark">Email Shelter</button>
-                          </a>
-                        </Accordion.Body>
-                      </Accordion.Item>
-                    </Accordion>
-                  </span>
+                      <button id="favourite" class="btn">
+                        <i class="fas fa-heart"></i>
+                      </button>
+                    </span>
+                    <span>
+                      <Accordion>
+                        <Accordion.Item eventKey="0">
+                          <Accordion.Header>
+                            <h6>More about {petsFromLocalStorage[key].name}</h6>
+                          </Accordion.Header>
+                          <Accordion.Body>
+                            <h3>{`🏡${petsFromLocalStorage[key].contact.address.city}, ${petsFromLocalStorage[key].contact.address.state}`}</h3>
+                            <h4>{`${petsFromLocalStorage[key].breeds} ${petsFromLocalStorage[key].type}`}</h4>
+
+                            <h5>{`${description}`}</h5>
+                            <br />
+                            <u>
+                              <b>Environment:</b>
+                            </u>
+                            <br />
+                            {kids}
+                            <br />
+                            {cats}
+                            <br />
+                            {dogs}
+                            <br />
+                            <br />
+                            <u>
+                              <b>Good to know:</b>
+                            </u>
+                            <br />
+                            {houseTrained}
+                            <br />
+                            {shotsCurrent}
+                            <br />
+                            {spayedNeutered}
+                            <br />
+                            {specialNeeds}
+                            <div class="map"></div>
+                            <a
+                              href={`mailto:${petsFromLocalStorage[key].contact.email}?subject=Potential%20Adoption&body=I%20would%20like%20to%20have%20more%20information%20about%20${petsFromLocalStorage[key].name}.`}
+                            >
+                              <button class="btn btn-dark">
+                                Email Shelter
+                              </button>
+                            </a>
+                          </Accordion.Body>
+                        </Accordion.Item>
+                      </Accordion>
+                    </span>
+                  </div>
                 </div>
-              </div>
-            </tr>
-          </>
-        );
-      })}
+              </tr>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
